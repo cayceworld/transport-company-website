5a38d219c880976712fc0a8167c8ebbd630a2336
Vladyslav Sydorenko <cayceworld@outlook.com>
2023-12-09 12:01:03 +0100

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,93 @@
+const app = {
+  initPages: function () {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector('#pages').children;
+    thisApp.navLinks = document.querySelectorAll('.nav a');
+
+    const idFromHash = window.location.hash.replace('#', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id == idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        thisApp.activatePage(id);
+
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle('active', page.id == pageId);
+    }
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle('active', link.getAttribute('href') == pageId);
+    }
+  },
+  initMenu: function () {
+    const thisApp = this;
+
+    thisApp.menu = document.querySelector('.burger');
+    thisApp.navigation = document.querySelector('.navigation');
+    thisApp.navLinks = document.querySelectorAll('.nav__link');
+
+
+    thisApp.menu.addEventListener('click', function (event) {
+      event.preventDefault();
+
+      thisApp.navigation.classList.toggle('active');
+    });
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        thisApp.navigation.classList.remove('active');
+      });
+    }
+  },
+  initAppearEffect: function () {
+    const thisApp = this;
+    thisApp.firstWord = document.querySelector('.first-word');
+    thisApp.secondWord = document.querySelector('.second-word');
+    console.log(thisApp.firstWord);
+    console.log(thisApp.secondWord);
+
+    function toggleSpans() {
+      // Toggle visibility
+      thisApp.firstWord.classList.toggle('hidden');
+      thisApp.secondWord.classList.toggle('hidden');
+    }
+
+    // Run toggleSpans every 5 seconds (5000 milliseconds)
+    toggleSpans();
+  },
+  init: function () {
+    const thisApp = this;
+
+    thisApp.initPages();
+    thisApp.initMenu();
+    //thisApp.initAppearEffect();
+    setInterval(() => thisApp.initAppearEffect(), 3000);
+  },
+};
+
+app.init();
