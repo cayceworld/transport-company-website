bacb518cba492e302a8b4a25c285c2013b0daff6
backupvs <dmitryholubev@gmail.com>
2023-12-14 18:12:17 +0200

    remove stripping slashes

--- src/php/mailer.php
+++ src/php/mailer.php
@@ -51,7 +51,6 @@
 
   function sanitizeInput($input) {
     $input = trim($input);
-    $input = stripslashes($input);
     $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
     return $input;
   }
